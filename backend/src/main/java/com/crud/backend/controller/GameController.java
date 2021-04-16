de34bb57fdfe59da3b75a71d33867a9e24da7fb3
Caterina <crbergas@gmail.com>
2021-04-16 21:05:48 +0200

    feat: annotations for controller and service

--- backend/src/main/java/com/crud/backend/controller/GameController.java
+++ backend/src/main/java/com/crud/backend/controller/GameController.java
@@ -3,7 +3,10 @@ package com.crud.backend.controller;
 import com.crud.backend.services.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GameController {
     
     @Autowired
--- backend/src/main/java/com/crud/backend/services/GameService.java
+++ backend/src/main/java/com/crud/backend/services/GameService.java
@@ -1,5 +1,8 @@
 package com.crud.backend.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
     
 }
