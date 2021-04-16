3db998e1a8c32ee420a7b9406c2f7db83d6e4ccd
Caterina <crbergas@gmail.com>
2021-04-16 12:46:23 +0200

    feat: frontend set up

--- backend/src/main/java/com/crud/backend/HomeController.java
+++ backend/src/main/java/com/crud/backend/HomeController.java
@@ -1,11 +1,13 @@
 package com.crud.backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @CrossOrigin
     @GetMapping("/")
     public String sayHi() {
         return "Hey There";
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return  {
+      title: ''
+    }
+  },
+  props: {
+    msg: String
+  },
+  mounted() {
+    fetch('http://localhost:8080')
+      .then( response => {
+        return response.text()
+      })
+      .then( data => {
+          this.title = data
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
