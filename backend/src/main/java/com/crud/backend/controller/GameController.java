1a1e2fd1af8db46f781608d2dad8955eb2e81898
Caterina <crbergas@gmail.com>
2021-04-17 08:53:45 +0200

    feat: list games

--- backend/src/main/java/com/crud/backend/controller/GameController.java
+++ backend/src/main/java/com/crud/backend/controller/GameController.java
@@ -1,16 +1,26 @@
 package com.crud.backend.controller;
-
-import com.crud.backend.services.GameService;
+import com.crud.backend.service.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.crud.backend.model.GameModel;
+import java.util.List;
+
+
 
 @RestController
 public class GameController {
     
     @Autowired
     private GameService gameService; 
+
+    @GetMapping("/getGames")
+    public List<GameModel> getGames() {
+       
+        return (List<GameModel>) gameService.listGames();
+
+    }
 
     
 }
--- backend/src/main/java/com/crud/backend/services/GameService.java
+++
@@ -1,8 +0,0 @@
-package com.crud.backend.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GameService {
-    
-}
