0cafed7b74d9ae3fada7863ac80f3538ca473415
Caterina <crbergas@gmail.com>
2021-04-17 09:12:44 +0200

    feat: CrossOrigin annotation added

--- backend/src/main/java/com/crud/backend/controller/GameController.java
+++ backend/src/main/java/com/crud/backend/controller/GameController.java
@@ -2,6 +2,7 @@ package com.crud.backend.controller;
 import com.crud.backend.service.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.crud.backend.model.GameModel;
@@ -16,6 +17,7 @@ public class GameController {
     private GameService gameService; 
 
     @GetMapping("/getGames")
+    @CrossOrigin
     public List<GameModel> getGames() {
        
         return (List<GameModel>) gameService.listGames();
