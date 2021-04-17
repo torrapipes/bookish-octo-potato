f5f24bcac10f605054859ced75ca10d633e548ee
Caterina <crbergas@gmail.com>
2021-04-17 08:51:55 +0200

    feat: service for game

---
+++ backend/src/main/java/com/crud/backend/service/GameService.java
@@ -0,0 +1,22 @@
+package com.crud.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import com.crud.backend.model.GameModel;
+import com.crud.backend.repository.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameModel> listGames() {
+        return (List<GameModel>) gameRepository.findAll();
+    }
+
+
+}
