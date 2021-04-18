c50d182b2f2d37cb2f289eadaef7ecdbfec002d1
Caterina <crbergas@gmail.com>
2021-04-18 20:48:21 +0200

    feat: put and delete routes

--- backend/src/main/java/com/crud/backend/controller/GameController.java
+++ backend/src/main/java/com/crud/backend/controller/GameController.java
@@ -4,8 +4,11 @@ import com.crud.backend.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.crud.backend.model.GameModel;
@@ -33,6 +36,22 @@ public class GameController {
     public GameModel createGame(@RequestBody GameModel game) {        
         return gameService.save(game);
     }
+
+    @PutMapping(
+        value = "/updateGame",
+        consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @CrossOrigin
+    public GameModel updateGame(@RequestBody GameModel game) {
+        return gameService.save(game);
+    }
+
+    @DeleteMapping("/deleteGame/{id}")
+    @CrossOrigin
+    public void deleteGame(@PathVariable("id") int id) {
+        gameService.delete(id);
+    }
+
 
     
 }
