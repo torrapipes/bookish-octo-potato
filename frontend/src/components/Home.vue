c31df78dc53775426fd8a1897455e24e9d5d0613
Caterina <crbergas@gmail.com>
2021-04-18 16:23:56 +0200

    feat: modal for creating game

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
+    <button @click="showModal = true">
+      <h2>NEW GAME</h2>
+    </button>
+    <Modal v-if="showModal" @success="showSuccessModal = true" @closeModal="showModal = false"></Modal>
+    <SuccessModal></SuccessModal>
     <Card v-for="game in games" v-bind:key="game.id" v-bind:object="game"></Card>
   </div>
 </template>
 
 <script>
 import Card from './Card.vue'
+import Modal from './Modal.vue'
+import SuccessModal from './SuccessModal.vue'
+
 export default {
   name: 'Home',
   data() {
     return  {
       title: 'GAMES',
-      games: ''
+      games: '',
+      showModal: false,
+      showSuccessModal: false
     }
   },
   components: {
-    Card
+    Card,
+    Modal,
+    SuccessModal
   },
   mounted() {
     // list games
@@ -46,6 +58,22 @@ export default {
     letter-spacing: 1rem;
     margin-bottom: 1rem;
   }
+  button {
+    margin: 2rem 0 2rem 0;
+    width: 30rem;
+    background: rgb(0, 0, 0);
+    font-family: 'Debug Free Trial';
+    font-size: 2rem;
+    transition: transform .2s;
+    padding: 0rem;
+    border-radius: 1rem;
+    border: none;
+    box-shadow: 10px 10px 24px -5px rgba(0,0,0,0.75);
+    color: rgb(247, 242, 203);;
+  }
+  button:hover {
+    transform: scale(1.1);
+  }
 }
 @media screen and (max-width: 450px) {
   .card {
@@ -54,6 +82,10 @@ export default {
   h1 {
     font-size: 5rem;
     letter-spacing: normal !important;
+  }
+  button {
+    width: 13rem !important;
+    font-size: 1.5rem !important;
   }
 }
 </style>
---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,215 @@
+<template>
+    <div>
+        <transition name="modal">
+            <div class="modal-mask">
+                <div class="modal-wrapper">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button class="button close" @click="closeModal(false)">
+                                    <i class="fas fa-times"/>
+                                </button>
+                                <h1 class="modal-title">Create a game</h1>
+                                <div class="modal-body">
+                                    <form class="form-group">
+                                        <!-- Sport -->
+                                        <div class="select">
+                                            <select v-model="sport">
+                                                <option disabled selected>Choose sport</option>
+                                                <option value="Basketball">Basketball</option>
+                                                <option value="Soccer">Soccer</option>
+                                                <option value="Ping Pong">Ping Pong</option>
+                                                <option value="Bowling">Bowling</option>
+                                                <option value="Football">Football</option>
+                                            </select>
+                                        </div>
+                                        <!-- Admin -->
+                                        <div>
+                                            <input type="text" placeholder="Your Name" v-model="admin" name="admin">
+                                        </div>
+                                        <!-- Place -->
+                                        <div>
+                                            <input type="text" placeholder="Location of the game" v-model="place" name="place">
+                                        </div>
+                                        <!-- Date -->
+                                        <div>
+                                            <label for="date">When are you playing?</label>
+                                            <tr/>
+                                            <input type="date" v-model="date" name="date">  
+                                        </div>
+                                    </form>
+                                </div>
+                                <div align="center">
+                                    <input class="button" @click="submit()" value="POST GAME">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        data() {
+            return {
+                admin: '',
+                sport: 'Choose sport',
+                place: '',
+                date: ''
+            }
+        },
+        methods: {
+            closeModal(value) {
+                this.$emit('closeModal', value)
+            },
+            submit() {
+                // building game object
+                let game = {
+                    'admin': this.admin,
+                    'sport': this.sport,
+                    'place': this.place,
+                    'date': this.date
+                }
+
+                const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(game)
+                };
+                fetch("http://localhost:8080/createGame", requestOptions)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log("Post response:", data)
+                        this.$emit('success', true)
+                    })
+                    .catch(error => {
+                        console.error('Erroor', error)
+                    }) 
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,.5);
+    display: table;
+    transition: opacity .3s ease;
+    .modal-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .modal-dialog {
+        background: white;
+        padding: 1rem;
+        border-radius: 1rem;
+        box-shadow: 10px 10px 24px -5px rgba(0,0,0,0.75);
+        .modal-title {
+            font-family: 'Debug Free Trial';
+            letter-spacing: .5rem;
+        }
+        .button {
+            margin-top: 1rem;
+            background: purple;
+            color: rgb(247, 242, 203);
+            border: none;
+            border-radius: 2rem;
+            text-align: center;
+            padding: 1rem;
+            font-family: 'Debug Free Trial';
+            font-size: 2rem;
+        }
+        .button:hover {
+            background: rgb(247, 242, 203);
+            color: purple;
+            cursor: pointer;
+        }
+        .close {
+            width: 1rem;
+            font-size: 1rem;
+            height: 1rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: auto; 
+        }
+        .form-group {
+            width: 30rem;
+            padding: 1rem;
+            display: flex;
+            flex-direction: column;
+            .select {
+                position: relative;
+                font-family: Arial;
+                width: 30rem;
+                select {
+                    width: 30rem;
+                    font-size: 1.5rem;
+                    color: gray;
+                    margin-bottom: 1rem;
+                    border: none;
+                    border-bottom: 1px solid black;
+                    option {
+                        font-family: FontAwesome, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                    }
+                }
+            }
+            input {
+                margin-bottom: 1rem;
+            }
+            input[type="text"] {
+                border: none;
+                border-bottom: 1px solid black;
+                width: 30rem;
+                font-size: 1.5rem;
+            }
+            label {
+                font-size: 1.5rem;
+            }
+            input[type="date"] {
+                font-size: 1rem;
+                margin-top: 1rem;
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 450px) {
+
+    .form-group {
+        width: 14rem !important;
+        .select {
+            font-size: 1rem !important;
+            width: 14rem !important;
+            select {
+                width: 14rem !important;
+                font-size: 1rem !important;
+            }
+        }
+        input[type="text"] {
+            font-size: 1rem !important;
+            width: 14rem !important;
+        }
+        label {
+            font-size: 1rem !important;
+        }
+    }
+    .button {
+        width: 13rem;
+    }
+
+}
+
+</style>
