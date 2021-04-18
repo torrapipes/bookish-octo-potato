a14e23a2e5b418396a16db0de58bda307851b43c
Caterina <crbergas@gmail.com>
2021-04-18 15:31:02 +0200

    feat: create game route

--- backend/src/main/java/com/crud/backend/controller/GameController.java
+++ backend/src/main/java/com/crud/backend/controller/GameController.java
@@ -2,8 +2,11 @@ package com.crud.backend.controller;
 import com.crud.backend.service.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.crud.backend.model.GameModel;
 import java.util.List;
@@ -18,10 +21,18 @@ public class GameController {
 
     @GetMapping("/getGames")
     @CrossOrigin
-    public List<GameModel> getGames() {
-       
+    public List<GameModel> getGames() {  
         return (List<GameModel>) gameService.listGames();
+    }
 
+    @PostMapping(
+        value = "/createGame",
+        consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @CrossOrigin
+    public String createGame(@RequestBody GameModel game) {
+        gameService.save(game);
+        return "hi";
     }
 
     
