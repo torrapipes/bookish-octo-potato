ffa247379cea55b1f76dca31dbdb80920e4f2c56
Caterina <crbergas@gmail.com>
2021-04-18 09:17:26 +0200

    style: whitespaces

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,6 +18,6 @@ export default {
 <style>
 body {
   background: rgb(2,0,36);
-background: linear-gradient(42deg, rgba(2,0,36,1) 0%, rgba(20,2,55,1) 0%, rgba(80,9,121,1) 35%, rgba(90,4,249,1) 97%);
+  background: linear-gradient(42deg, rgba(2,0,36,1) 0%, rgba(20,2,55,1) 0%, rgba(80,9,121,1) 35%, rgba(90,4,249,1) 97%);
 }
 </style>
